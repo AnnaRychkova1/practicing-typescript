a39ac8305e032ac50f7288c5a5519568249ea7f8
AnnaRychkova1 <aanytkaa@gmail.com>
2024-04-18 21:01:14 +0100

    corrections

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 async function fetchData<T>(url: string): Promise<T> {
   try {
-    const response = await axios.get(url);
+    const response = await axios.get<T>(url);
     return response.data;
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
   }
 }
 
-fetchData('https://jsonplaceholder.typicode.com/todos/1');
+fetchData<object[]>('https://jsonplaceholder.typicode.com/todos/1');
